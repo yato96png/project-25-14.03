57fb5a01468382e244c1d953f271f7f710bc339a
vizal123 <yato9png@gmail.com>
2025-03-17 13:22:13 +0500

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  background: #007bff;
+  color: white;
+  padding: 15px;
+  text-align: center;
+  font-size: 20px;
+`;
+
+const Header = () => <HeaderContainer>My Twitter Clone</HeaderContainer>;
+
+export default Header;
